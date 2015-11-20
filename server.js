ad53ee1d6e20b32b3c2b4663972bd2154875b7a5
Chris James <qui666@gmail.com>
2015-11-20 16:22:13 +0000

    Using request promise

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 "use strict";
 
 var express = require('express');
-var request = require('request');
+var request = require('request-promise');
 var redis = require("redis");
 
 const redisClient = redis.createClient('6379', 'redis');
@@ -10,32 +10,14 @@ const mjURL = 'http://fake-intel:9090/chrisrules';
 
 redisClient.set('some key', 'Chris is the best');
 
-app.use((_, res, next) =>{
-    res.setHeader('content-type', 'text/javascript');
-    next();
-});
-
 app.get('/', (req, res) => {
-
-    callAPI()
+    request(mjURL)
         .then(body => res.send(body))
-        .catch(err =>res.send("Uh oh"));
+        .then(res.setHeader('content-type', 'text/javascript'))
+        .catch(err => res.send("Uh oh"));
 
     redisClient.get('some key', (err, reply) => console.log('Got this important information from redis', reply));
-
 });
-
-function callAPI(){
-    return new Promise((resolve, reject) =>{
-        request(mjURL, (error, response, body) => {
-            if(!error){
-                resolve(body);
-            }else{
-                reject(error);
-            }
-        });
-    });
-}
 
 const PORT = 8080;
 app.listen(PORT);
