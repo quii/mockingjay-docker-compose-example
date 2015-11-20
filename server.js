bcc39236af61ce1beab8de2a9f31561ca25298c4
Chris James <qui666@gmail.com>
2015-11-20 16:14:48 +0000

    Promisey

--- server.js
+++ server.js
@@ -10,25 +10,32 @@ const mjURL = 'http://fake-intel:9090/chrisrules';
 
 redisClient.set('some key', 'Chris is the best');
 
-app.use((req, res, next) =>{
+app.use((_, res, next) =>{
     res.setHeader('content-type', 'text/javascript');
     next();
-})
+});
 
 app.get('/', (req, res) => {
 
-    request(mjURL, (error, response, body) => {
+    callAPI()
+        .then(body => res.send(body))
+        .catch(err =>res.send("Uh oh"));
 
-        if (!error) {
-            res.send(body);
-        } else {
-            res.send('Couldnt reach MockingJay at', mjURL);
-        }
-
-        redisClient.get('some key', (err, reply) => console.log('Got this important information from redis', reply));
-    });
+    redisClient.get('some key', (err, reply) => console.log('Got this important information from redis', reply));
 
 });
+
+function callAPI(){
+    return new Promise((resolve, reject) =>{
+        request(mjURL, (error, response, body) => {
+            if(!error){
+                resolve(body);
+            }else{
+                reject(error);
+            }
+        });
+    });
+}
 
 const PORT = 8080;
 app.listen(PORT);
